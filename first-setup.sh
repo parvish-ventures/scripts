53bd4729757d644ac6d960275355b8e8e02b8640
Ubuntu <ubuntu@ip-172-31-7-212.ap-south-1.compute.internal>
2024-05-11 07:28:53 +0000

    revert

--- first-setup.sh
+++ first-setup.sh
@@ -18,7 +18,7 @@ echo "--------------------------------------------------------------------------
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install --upgrade
+sudo ./aws/install
 
 ####################################################
 #### Setup Node
