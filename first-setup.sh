0efe8687a4f0e8698863677c7598904e605cb005
Ubuntu <ubuntu@ip-172-31-7-212.ap-south-1.compute.internal>
2024-05-11 07:28:14 +0000

    fixed aws cli for upgrade

--- first-setup.sh
+++ first-setup.sh
@@ -18,7 +18,7 @@ echo "--------------------------------------------------------------------------
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --upgrade
 
 ####################################################
 #### Setup Node
